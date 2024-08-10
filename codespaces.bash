b1721dbcf5f2599db841b8e14cb94181ba264fa8
Durai Shanmugaraj R <68941801+duraishanmugaraj@users.noreply.github.com>
2024-08-10 18:19:08 +0000

    codespaces

--- codespaces.bash
+++ codespaces.bash
@@ -1,3 +1,7 @@
+#!/bin/sh
+#bash codespaces.bash
+source /usr/local/sdkman/bin/sdkman-init.sh
+
 sdk install java 8.0.422-amzn
 sdk default java 8.0.422-amzn
 sdk install spark 3.0.1
@@ -6,7 +10,6 @@ pip install snowflake-connector-python load_dotenv
 
 
 #Install aws cli
-sudo yum remove awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin
